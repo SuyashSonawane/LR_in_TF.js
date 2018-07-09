736bb02f6a8d3f048963efa536269a1b86c3abaf
Suyash Sonawane <suyashsonawane007@gmail.com>
2018-07-09 16:14:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const lrate = 0.5;
 const optmz = tf.train.sgd(lrate)
 
 function setup(){
-  var canvas = createCanvas(windowWidth, windowHeight);
+  var canvas =createCanvas(window.innerWidth, window.innerHeight);
   canvas.parent('canvas')
   background('black');
   m = tf.variable(tf.scalar(random(10)));
